6d057ea20cdd976f32fe8a25dbd33902f9f78162
satoRoger <pyomta13@gmail.com>
2020-10-09 11:12:09 +0900

    test追加

--- src/Domain/attendanceManagement/tests/employee/employeeId.test.ts
+++ src/Domain/attendanceManagement/tests/employee/employeeId.test.ts
@@ -1,9 +1,15 @@
 import { EmployeeId } from "../../timecard/valueObjects";
 
 describe("employeeIdのテスト", (): void => {
-  test("EmployeIdが正しく取得できるか", () => {
+  test("idが正しく取得できるか", () => {
     const id = "test01";
     const employeeId = new EmployeeId(id);
     expect(employeeId.getId()).toBe(id);
+  });
+  
+  test("空文字idは拒否するか", () => {
+    const id = "";
+    const employeeId = new EmployeeId(id);
+    expect(employeeId.getId()).toThrow(id);
   });
 });
