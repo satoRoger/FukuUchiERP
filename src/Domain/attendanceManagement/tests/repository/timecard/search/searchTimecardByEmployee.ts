e6df70e352a86512717ff39b34cd1dd86902cb92
satoRoger <pyomta13@gmail.com>
2020-10-13 10:31:47 +0900

    add tests

---
+++ src/Domain/attendanceManagement/tests/repository/timecard/search/searchTimecardByEmployee.ts
@@ -0,0 +1,32 @@
+describe('searchTimecardByEmployee', () => {
+	let seachTimecard ;
+	let employee1;
+	let employee2;
+	beforeAll(()=>{
+		const repository = new EntityRepository().getTimecardRepository();
+		seachTimecard = new SearchTimecardByEmployee(repository);
+		
+		employee1 = new EntityFactory.createEmployee("test01");
+		employee2 = new EntityFactory.createEmployee("test02");
+		
+		const specification = new PunchSpecification().getAttendance();
+		
+		const command1 = new PunchCommand().createAttendance
+		(new Date(2020,10,10),repository,specification,new Coordinate(20,20));
+		
+		const command2 = new PunchCommand().createAttendance
+		(new Date(2020,10,20),repository,specification,new Coordinate(10,20));
+		
+		const command3 = new PunchCommand().createAttendance
+		(new Date(2020,8,10),repository,specification,new Coordinate(20,28));
+		
+		employee1.punchCommand(command1);
+		employee1.punchCommand(command2);
+		employee2.punchCommand(command3);
+		
+	})
+	
+	test('search', () => {
+		seachTimecard.search(employee1)
+	})
+})
--- src/Domain/attendanceManagement/tests/timecard/timecard.test.ts
+++ src/Domain/attendanceManagement/tests/timecard/timecard.test.ts
@@ -1,6 +1,41 @@
 describe('timecard', () => {
-  test('timecardが正常に作られるか', () => {
-    expect(3).toBe(3);
-  })
-  
+	let punchDate;
+	let employeeId;
+	let coodinate;
+	let cardType;
+	let timecard;
+	
+	beforeEach(() => {
+		employeeId = new EmployeeId("test01");
+		punchDate = new Date(2020,10,10,5,5,5);
+		coordinate = new Coordinate(20,20);
+		cardType = CardType.attendance;
+		timecard = new Timecard(employeeId,punchDate,cardType,coordinate);
+		timecardWithoutCoordinate = new Timecard(employeeId,punchDate,cardType);
+	})
+	
+	test('getCoordinate', () => {
+	  expect(timecard.getCoordinate().equal(coodinate)).toBe(true);
+	})
+	test('getEmployeeId', () => {
+	  expect(timecard.getEmployeeId().equal(employeeId)).toBe(true);
+	})
+	test('isCardType', () => {
+	  expect(timecard.isCardType(CardType.attendance)).toBe(true);
+	  expect(timecard.isCardType(CardType.leaveWork)).toBe(false);
+	  expect(timecard.isCardType(CardType.takeBreak)).toBe(false);
+	  expect(timecard.isCardType(CardType.endBreak)).toBe(false);
+	})
+	test('isPunchAfter', () => {
+	  expect(timecard.isPunchAfter(new Date(2020,10,10,4,5,5))).toBe(true);
+	  expect(timecard.isPunchAfter(new Date(2020,10,6,5,5,5))).toBe(true);
+	  expect(timecard.isPunchAfter(new Date(2020,10,14,4,5,5))).toBe(false);
+	  expect(timecard.isPunchAfter(new Date(2020,10,10,5,5,5))).toBe(true);
+	})
+	test('isPunchBefore', () => {
+	  expect(timecard.isPunchBefore(new Date(2020,10,10,4,5,5))).toBe(false);
+	  expect(timecard.isPunchBefore(new Date(2020,10,6,5,5,5))).toBe(false);
+	  expect(timecard.isPunchBefore(new Date(2020,10,14,4,5,5))).toBe(true);
+	  expect(timecard.isPunchBefore(new Date(2020,10,10,5,5,5))).toBe(true);
+	})
 })
