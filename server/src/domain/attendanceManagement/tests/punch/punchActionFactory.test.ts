c81ac615a1b81aeb30455f2c7cfb23c34c3246be
satoRoger <pyomta13@gmail.com>
2020-10-28 07:43:56 +0900

    punchActionFactory

---
+++ server/src/domain/attendanceManagement/tests/punch/punchActionFactory.test.ts
@@ -0,0 +1,7 @@
+
+describe('punchFactory', () => {
+	test("actionAttendance",()=>{
+		const action = new PunchActionFactory().actionAttendance();
+		expect(action instanceof PunchAttendanceAction).toBe(true);
+	});
+})
