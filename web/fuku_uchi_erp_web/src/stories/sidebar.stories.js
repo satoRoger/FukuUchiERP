ef728c6902e262e2795c89163408415d609c7243
satoRoger <pyomta13@gmail.com>
2020-11-11 11:57:33 +0900

    sidebar stroy

--- web/fuku_uchi_erp_web/src/stories/sidebar.stories.js
+++ web/fuku_uchi_erp_web/src/stories/sidebar.stories.js
@@ -1,42 +1,50 @@
-import { stringify } from "querystring";
-import React from "react";
-import Sidebar from "../view/component/sidebar/sidebar";
-import { containerProps } from "../view/component/sidebar/sidebarContainer";
+import { stringify } from 'querystring';
+import React from 'react';
+import Sidebar from '../view/component/sidebar/sidebar';
+import { containerProps } from '../view/component/sidebar/sidebarContainer';
 import { createMuiTheme, makeStyles, ThemeProvider } from '@material-ui/core/styles';
 import { orange } from '@material-ui/core/colors';
 
 export default {
-  title: "WIP/component/sidebar"
+	title: 'WIP/component/sidebar',
+	argTypes: {
+		textColor: { control: 'color' },
+		backgroundColor: { control: 'color' },
+		activeTextColor: { control: 'color' },
+		activeBackgroundColor: { control: 'color' },
+	},
 };
 
-const theme = createMuiTheme({
-  palette: {
-	  color:{
-		white: orange[500],
+const template = (args) => {
+	const theme = {
+		palette: {
+			color: {
+				white: args.textColor,
+			},
+			main: {
+				darker: args.activeBackgroundColor,
+			},
 		},
-	main:{
-		darker:orange[500],
-	}
-  }
-});
+	};
 
-const template = (args) => (
-  <>
-  <ThemeProvider theme={theme}>
-    <Sidebar.container open={args.open}>
-      <Sidebar.list>
-        <Sidebar.link text={args.text} active/>
-        <Sidebar.link text={"test"} />
-        <Sidebar.link text={"test"} />
-        <Sidebar.link text={"test"} />
-        <Sidebar.link text={"test"} />
-        <Sidebar.link text={"test"} />
-      </Sidebar.list>
-    </Sidebar.container>
-	</ThemeProvider>
-  </>
-);
+	return (
+		<>
+			<ThemeProvider theme={createMuiTheme(theme)}>
+				<Sidebar.container open={args.open}>
+					<Sidebar.list>
+						<Sidebar.link text={args.text} active />
+						<Sidebar.link text={'test'} />
+						<Sidebar.link text={'test'} />
+						<Sidebar.link text={'test'} />
+						<Sidebar.link text={'test'} />
+						<Sidebar.link text={'test'} />
+					</Sidebar.list>
+				</Sidebar.container>
+			</ThemeProvider>
+		</>
+	);
+};
 
 export const basicSidebar = template.bind({});
 
-basicSidebar.args = { open: false ,text:"tete"};
+basicSidebar.args = { open: false, text: 'tete' };
