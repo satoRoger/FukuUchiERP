f461c092126590a217bffffd3b8c9b5d45f24246
satoRoger <pyomta13@gmail.com>
2020-12-14 10:18:56 +0900

    database

---
+++ functions/src/database/entities/timecard.ts
@@ -0,0 +1,31 @@
+import { Column, Entity, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export default class TimecardModel {
+  @PrimaryGeneratedColumn()
+  public id: number;
+
+  @Column({ type: "timestamp"})
+  public date: DateTime;
+
+  @Column({ type: "varchar" })
+  public cardType: string ;
+  
+  @OneToOne(type => User,user => user.id)
+  public userId:number
+
+  @Column({ type: "double precision" })
+  public longitude?: number ;
+  
+  @Column({ type: "double precision" })
+  public latitude?: number ;
+  
+  
+  constructor(date:DateTime,cardType:string,userId:number,longitude?:number,latitude?:number){
+	  this.date=date;
+	  this.cardType=cardType;
+	  this.userId=userID;
+	  this.longitude=longitude;
+	  this.laittude=latitude
+  }
+}
