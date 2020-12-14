69ddf123126b7666d6f6cea4ba56aa2317272d0d
satoRoger <pyomta13@gmail.com>
2020-12-15 07:33:57 +0900

    entity

--- functions/src/database/entities/timecard.ts
+++ functions/src/database/entities/timecard.ts
@@ -1,31 +1,32 @@
+import { DateTime } from "luxon";
 import { Column, Entity, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
-export default class TimecardModel {
+export default class Timecard {
   @PrimaryGeneratedColumn()
-  public id: number;
+  public id?: number;
 
-  @Column({ type: "timestamp"})
+  @Column({ type: "timestamp" })
   public date: DateTime;
 
   @Column({ type: "varchar" })
   public cardType:  ;
-  
-  @OneToOne(type => User,user => user.id)
-  public userId:number
+
+  @OneToOne(type => User, user => user.id)
+  public userId: number
 
   @Column({ type: "double precision" })
-  public longitude?: number ;
-  
+  public longitude?: number;
+
   @Column({ type: "double precision" })
-  public latitude?: number ;
-  
-  
-  constructor(date:DateTime,cardType:string,userId:number,longitude?:number,latitude?:number){
-	  this.date=date;
-	  this.cardType=cardType;
-	  this.userId=userID;
-	  this.longitude=longitude;
-	  this.laittude=latitude
+  public latitude?: number;
+
+
+  constructor(date: DateTime, cardType: string, userId: number, longitude?: number, latitude?: number) {
+    this.date = date;
+    this.cardType = cardType;
+    this.userId = userID;
+    this.longitude = longitude;
+    this.laittude = latitude
   }
 }
