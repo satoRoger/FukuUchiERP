005173e173daf3f3ca6bb599ac3f724def97de7e
nobuaki <satounbd@nttdata-tokai.co.jp>
2020-12-21 14:28:09 +0900

    post workflows

--- functions/src/controller/workflows/frontRouting/postWorkflows.ts
+++ functions/src/controller/workflows/frontRouting/postWorkflows.ts
@@ -1,1 +1,23 @@
-export default class PostWorkflows {}
+export default async function PostWorkflows(req: express.Request,
+  res: express.Response) {
+
+let type:string|undefined;
+let vacationDate:DateTime|undefined;
+
+const request = JSON.perse(req.body);
+
+if(typeof request.type === "string"){
+type = request.type;
+}
+if(typeof request.vacationDate === "string"){
+vacationDate = request.vacationDate;
+}
+
+const postParam = new ValidateWorkflowsPostParam(type,vacationDate).createWithValid();
+
+if(postParam){
+const response = await PostWorkflowsRouter(postParam);
+res.json(response.perse());
+}
+
+}
