30cebd12e05a6aa9541b6ec7fb405b7e96dcfb37
nobu <pyomta13@gmail.com>
2021-01-02 15:07:01 +0900

    facility

--- functions/src/interactor/src/facilities/postFacilities.ts
+++ functions/src/interactor/src/facilities/postFacilities.ts
@@ -1,39 +1,25 @@
 import container from "../../../util/di/inversify.config";
 import Types from "../../../util/di/types";
-import EventRepository from "../../../domain/eventManager/src/repository/event/eventRepository";
-import EventsPostParam from "../InteractorObject/events/eventsPostParam";
-import EventAPIInterface from "../APIInterface/event/event";
-import EventFactory from "../../../domain/eventManager/src/entity/event/eventFactory";
+import FacilityFactory from "../../../domain/resourceManager/src/entity/facility/facilityFactory";
+import FacilitiesPostParam from "../InteractorObject/facilities/facilitiesPostParam";
+import FacilityAPIInterface from "../APIInterface/facility/facility";
+import FacilityRepository from "../../../domain/resourceManager/src/repository/facilityRepository";
 
 export default async function PostFacilities(
   param: FacilitiesPostParam
 ): Promise<FacilityAPIInterface[]> {
-  const repository = container.get<FacilityRepository>(Types.FacilitytRepository);
-
-  const newData = await repository.save(
-    new EventFactory().create(
-      "empty",
-      param.type,
-      param.start,
-      param.end,
-      param.title,
-      param.userId,
-      param.facilityIds
-    )
+  const repository = container.get<FacilityRepository>(
+    Types.FacilityRepository
   );
 
-  const response: EventAPIInterface = {
-    id: newData.id.value,
-    start: newData.start,
-    end: newData.end,
-    title: newData.title.value,
-    type: newData.type,
-    facilityIds: newData.facilityIds?.map((id) => {
-      return id.value;
-    }),
-    userId: newData.employeeId?.value,
-  };
+  const newData = await repository.save(
+    new FacilityFactory().create("empty", param.name)
+  );
 
-  const result: EventAPIInterface[] = [response];
+  const response: FacilityAPIInterface = {
+    id: newData.id.value,
+    name: newData.name?.value,
+  };
+  const result: FacilityAPIInterface[] = [response];
   return result;
 }
