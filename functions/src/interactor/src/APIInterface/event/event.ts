b41247b35674ddb607af013cc0b00712c7136b40
nobu <pyomta13@gmail.com>
2021-01-03 14:44:26 +0900

    event

--- functions/src/interactor/src/APIInterface/event/event.ts
+++ functions/src/interactor/src/APIInterface/event/event.ts
@@ -1,10 +1,11 @@
 import { DateTime } from "luxon";
+import EventType from "../../../../domain/eventManager/src/valueObject/eventType";
 export default interface EventAPIInterface {
   id: string;
   start: DateTime;
   end: DateTime;
   title: string;
-  type: string;
+  type: EventType;
   userId?: string;
   facilityIds?: string[];
 }
