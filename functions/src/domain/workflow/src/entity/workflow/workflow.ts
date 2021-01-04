798802e0b6b9588120048e8f721b72d07599cde3
nobuaki <satounbd@nttdata-tokai.co.jp>
2021-01-04 15:41:27 +0900

    workflow

--- functions/src/domain/workflow/src/entity/workflow/workflow.ts
+++ functions/src/domain/workflow/src/entity/workflow/workflow.ts
@@ -20,49 +20,40 @@ export default class Workflow {
   public get approversId(): ApproversId {
     return this._approversId;
   }
-  public set approversId(value: ApproversId) {
-    this._approversId = value;
-  }
   public get id(): WorkflowId {
     return this._id;
-  }
-  public set id(value: WorkflowId) {
-    this._id = value;
   }
   public get vacationDate(): DateTime | undefined {
     return this._vacationDate;
   }
-  public set vacationDate(value: DateTime | undefined) {
-    this._vacationDate = value;
-  }
   public get type(): WorkflowType {
     return this._type;
-  }
-  public set type(value: WorkflowType) {
-    this._type = value;
   }
   public get state(): WorkflowState {
     return this._state;
   }
-  public set state(value: WorkflowState) {
-    this._state = value;
-  }
   public get petitionDate(): DateTime {
     return this._petitionDate;
-  }
-  public set petitionDate(value: DateTime) {
-    this._petitionDate = value;
   }
   public get index(): WorkflowIndex {
     return this._index;
   }
-  public set index(value: WorkflowIndex) {
-    this._index = value;
-  }
   public get dtafter(): Employee {
     return this._dtafter;
   }
-  public set dtafter(value: Employee) {
-    this._dtafter = value;
+  
+  public get complete():boolean{
+	return this._state === WorkflowState.comlete;
+  }	  
+  
+  public approve(){
+	  if(this._index + 1 === this._approversId.length()){
+		  this._state = WorkflowState.comlete;
+	  }else{
+		  this._index++;
+	  }
+  }
+  public deny(){
+	 this._state = WorkflowState.deny
   }
 }
