6e8c6759e2e7db503816ce53286df08f0f5abf1f
nobu <pyomta13@gmail.com>
2021-02-14 20:05:20 +0900

    fullname

--- functions/src/interactor/src/APIInterface/user/fullname.ts
+++ functions/src/interactor/src/APIInterface/user/fullname.ts
@@ -1,13 +1,13 @@
 import Fullname from "../../../../domain/resourceManager/src/valueObject/fullname";
-import {
-  isString,
-} from "../../../../util/isType/isType";
+import { isString } from "../../../../util/isType/isType";
 
 export default class FullnameAPIInterface {
   readonly familyName?: string;
   readonly givenName?: string;
 
-  static fromDomainFullname(fullname: Fullname) {
+  static fromDomainFullname(fullname: Fullname): FullnameAPIInterface {
+    console.log(fullname.familyName);
+    console.log(fullname.familyName.value);
     return new FullnameAPIInterface(
       fullname.familyName.value,
       fullname.givenName.value
@@ -15,6 +15,7 @@ export default class FullnameAPIInterface {
   }
 
   constructor(familyName: any, givenName: any) {
+    console.log(typeof familyName);
     if (isString(familyName)) {
       this.familyName = familyName;
     }
